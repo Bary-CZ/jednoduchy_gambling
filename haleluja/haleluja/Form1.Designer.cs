3066e86cae5595329cf8ffb4436ad4e57b184835
Bary <61382585+Bary-CZ@users.noreply.github.com>
2025-03-09 23:58:28 +0100

    -minor updates
    +konto

--- haleluja/haleluja/Form1.Designer.cs
+++ haleluja/haleluja/Form1.Designer.cs
@@ -37,15 +37,21 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(12, 21);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(297, 173);
+            this.button1.Size = new System.Drawing.Size(152, 124);
             this.button1.TabIndex = 0;
-            this.button1.Text = "random cislo";
+            this.button1.Text = "Roztoč automat";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -53,7 +59,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(205, 279);
+            this.label1.Location = new System.Drawing.Point(217, 247);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 32);
             this.label1.TabIndex = 1;
@@ -63,7 +69,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(315, 230);
+            this.label2.Location = new System.Drawing.Point(309, 198);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 32);
             this.label2.TabIndex = 2;
@@ -72,7 +78,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(12, 230);
+            this.label3.Location = new System.Drawing.Point(12, 198);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(168, 32);
             this.label3.TabIndex = 3;
@@ -82,7 +88,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(235, 230);
+            this.label4.Location = new System.Drawing.Point(235, 198);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(80, 32);
             this.label4.TabIndex = 4;
@@ -92,7 +98,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(173, 230);
+            this.label5.Location = new System.Drawing.Point(170, 198);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(21, 32);
             this.label5.TabIndex = 5;
@@ -102,7 +108,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(12, 279);
+            this.label6.Location = new System.Drawing.Point(12, 247);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(114, 32);
             this.label6.TabIndex = 6;
@@ -112,17 +118,81 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label7.Location = new System.Drawing.Point(121, 279);
+            this.label7.Location = new System.Drawing.Point(121, 247);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(43, 32);
             this.label7.TabIndex = 7;
             this.label7.Text = "wr";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(170, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 32);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "konto:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(254, 62);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 37);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(343, 68);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 25);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "KČ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(15, 158);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(138, 16);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "zadej počáteční konto";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 30);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "zadej ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 360);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(381, 309);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -133,6 +203,7 @@
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "gamble";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -149,6 +220,12 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- haleluja/haleluja/Form1.cs
+++ haleluja/haleluja/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -13,13 +14,24 @@ namespace haleluja
 {
     public partial class Form1 : Form
     {
-        int rnd, cislo, vyhra, prohra, kol, winrate;
-       
+        int rnd, cislo, vyhra, prohra, kol, winrate, penize;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            penize = Convert.ToInt32(textBox1.Text);
+            label9.Text = penize.ToString();
+            label11.Visible = false;
+            button2.Visible = false;
+            textBox1.Visible = false;
+            label9.Visible = true;
+        }   
+
         string pocet;
         public Form1()
         {
             InitializeComponent();
             kol = 0;
+            label9.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
